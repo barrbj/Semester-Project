7a8ba38de8ddfb86cb22b984849645259bcb96ce
Mastef_Chief <mastefchief@gmail.com>
2019-04-07 10:53:54 -0400

    Added Skeleton Controller for FXML GUI

--- src/Cards24Controller.java
+++ src/Cards24Controller.java
@@ -1,5 +1,32 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Pane;
+
 public class Cards24Controller {
 
+    @FXML
+    private Button findASolutionBtn;
 
+    @FXML
+    private Button refreshBtn;
+
+    @FXML
+    private Button verifyBtn;
+
+    @FXML
+    private TextField expressionTextField;
+
+    @FXML
+    private TextField solutionTextField;
+
+    @FXML
+    private Pane cardPane;
+
+    @FXML
+    void onKeyTyped(KeyEvent event) {
+
+    }
 
 }
