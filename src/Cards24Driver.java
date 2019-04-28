809949f8683dafb2eee0b9fcd756cacf885c7e4e
Mastef_Chief <mastefchief@gmail.com>
2019-04-28 17:21:24 -0400

    Update Cards24Driver.java

--- src/Cards24Driver.java
+++ src/Cards24Driver.java
@@ -6,10 +6,19 @@ import javafx.stage.Stage;
 
 public class Cards24Driver extends Application {
 
+    private static Stage primaryStage;
+
+    public static Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("Cards24.fxml"));
-        primaryStage.setTitle("Cards24 Game");
+    public void start(Stage primaryStage) throws Exception {
+        this.primaryStage = primaryStage;
+        FXMLLoader menu = new FXMLLoader(getClass().getResource("Cards24Menu.fxml"));
+        Parent root = menu.load();
+        primaryStage.setTitle("Cards24");
+        primaryStage.setResizable(false);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
@@ -18,4 +27,5 @@ public class Cards24Driver extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
 }
